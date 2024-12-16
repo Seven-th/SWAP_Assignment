0d67e52eb9087b8ad8007fce47ec0d765c2e3f89
Seven-th <syedhyk3l@gmail.com>
2024-12-16 23:37:58 +0800

    changes made based on Aqeel's db code
    made some slight chnages to the main code based on the one Aqeel made. should be a lot bettter and more detailed-ish now igs idk tbh still needs some more additions and ironing out i feel

--- db.sql
+++ db.sql
@@ -5,24 +5,30 @@ CREATE DATABASE UserManagement;
 USE UserManagement;
 
 
+CREATE TABLE permission (
+    permission_id INT AUTO_INCREMENT PRIMARY KEY,
+    role ENUM('Admin', 'Researcher', 'Research Assistant') NOT NULL,
+    permission VARCHAR(255) NOT NULL,
+);
+
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    department_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(100) NOT NULL,
     head_user_id INT NOT NULl FOREIGN KEY REFERENCES users(id)
 )
 
-CREATE TABLE researchers (
+CREATE TABLE researcher (
     researcher_id INT AUTO_INCREMENT PRIMARY KEY,    
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,       
     country_code VARCHAR(5) DEFAULT '+65' NOT NULL,
     phone_number VARCHAR(15) NOT NULL,
     --> if can have like a dropdown list of all country codes for user to choose from 
-    --> to implement in the html, can try useing Int-Tel-input library idk how it works tho so ,')
+    --> to implement in the html, can try using Int-Tel-input library idk how it works tho so ,')
     --> else, keep to original  " phone_number VARCHAR(20) NOT NULL "
     password VARCHAR(255) NOT NULL,           
-    role ENUM('Admin', 'Researcher', 'Assistant_Researcher') DEFAULT 'Assistant_Researcher' NOT NULL,
-    department_id INT NULL FOREIGN KEY REFERENCES department(id)
+    permission_id INT NOT NUll FOREIGN KEY REFERENCES permission(permission_id),
+    department_id INT NOT NULL FOREIGN KEY REFERENCES department(department_id)
 )
 
 CREATE TABLE equipment_inventory (
@@ -33,31 +39,32 @@ CREATE TABLE equipment_inventory (
     restock_level INT NOT NULL --> Min merch for said product
 )
 
-CREATE TABLE equipment_usage_log (
-    log_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    equipment_id INT NOT NULL FOREIGN KEY REFERENCES equipment_inventory(equipment_id),
-    last_used_by INT NOT NULL FOREIGN KEY REFERENCES researchers(id),
-    department_id INT NOT NULL FOREIGN KEY REFERENCES department(id),
-    quantity INT NOT NULL, --> How much of this equipment did the researcher take
-)
-
 CREATE TABLE project ( --> used to be reports
     project_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL, --> like assignment specs for the project
+    funding DECIMAL(10, 2),
+    status ENUM('Ongoing', 'Completed') DEFAULT 'Ongoing',
     project_priority_level ENUM('Low', 'Medium', 'High') NOT NULL,
-    generated_by INT NOT NULL FOREIGN KEY REFERENCES researchers(id),
-    assigned_to INT NOT NULL FOREIGN KEY REFERENCES researchers(id),
-    department_id INT NOT NULL FOREIGN KEY REFERENCES department(id),
-    data_points TEXT NOT NULL, --> like assignment specs for the project
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+    generated_by INT NOT NULL FOREIGN KEY REFERENCES researcher(researcher_id),
+    assigned_to INT NOT NULL FOREIGN KEY REFERENCES researcher(researcher_id),
+    department_id INT NOT NULL FOREIGN KEY REFERENCES department(department_id),
+    team_members TEXT, --> fk to assistant researcher / researcher
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
+CREATE TABLE password_reset_request (
+    request_id INT AUTO_INCREMENT PRIMARY KEY,
+    researcher_id INT NOT NULL FOREIGN KEY REFERENCES researcher(researcher_id)
 )
 
 CREATE TABLE project_activity_log (
     log_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     project_id INT NOT NULL FOREIGN KEY REFERENCES project(project_id),
-    project_status ENUM('Initialized', 'Ongoing', 'Completed') NOT NULL,
-    last_updated_by INT NOT NULL FOREIGN KEY REFERENCES researchers(id),
-    department_id INT NOT NULL FOREIGN KEY REFERENCES department(id),
+    project_status ENUM('Ongoing', 'Completed') NOT NULL DEFAULT 'Ongoing',
+    last_updated_by INT NOT NULL FOREIGN KEY REFERENCES researcher(researcher_id),
+    department_id INT NOT NULL FOREIGN KEY REFERENCES department(department_id),
     data_points TEXT NOT NULL, --> will display current progress of the project
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL --> last edited time lah or smth like that
 )
@@ -65,8 +72,16 @@ CREATE TABLE project_activity_log (
 --> all in one audit log? like for login attempts/new users/project creation deletion etc...?
 CREATE TABLE audit_logs ( 
     log_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    researcher_id INT NOT NULL FOREIGN KEY REFERENCES researchers(id),
+    researcher_id INT NOT NULL FOREIGN KEY REFERENCES researcher(researcher_id),
     audit_action TEXT NOT NULL,
     audit_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     details TEXT NOT NULL
+)
+
+CREATE TABLE equipment_usage_log (
+    log_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    equipment_id INT NOT NULL FOREIGN KEY REFERENCES equipment_inventory(equipment_id),
+    last_used_by INT NOT NULL FOREIGN KEY REFERENCES researcher(researcher_id),
+    department_id INT NOT NULL FOREIGN KEY REFERENCES department(department_id),
+    quantity INT NOT NULL, --> How much of this equipment did the researcher take
 )
