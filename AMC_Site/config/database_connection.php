a9b1d418e7697683eae76c5e6a8eaf1b64d5bc4b
Seven-th <syedhyk3l@gmail.com>
2024-12-18 22:05:08 +0800

    attemp 1 for login page

--- AMC_Site/config/database_connection.php
+++ AMC_Site/config/database_connection.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 $host = 'localhost';           
 $db = 'your_database_name';   
 $user = 'root';                
@@ -19,5 +22,40 @@ try {
 } catch (PDOException $e) {
     // Handle connection error
     die("Database connection failed: " . $e->getMessage());
+}
+
+// Handle form submission of login page
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    //check if inputs are empty
+    if (empty($email) || empty(password)) {
+        header("Location: http://localhost/SWAP_Assignment/AMC_Site/public/login.php=?error=Both fields are required");
+        exit;
+    }
+
+    // Fetches data from database
+    $smtm = $pdo->prepare("SELECT * FROM researcher WHERE email = :email");
+    $smtm->execute(['email' => $email]);
+    $user = $smtm->fetch();
+
+    //verifying password
+    if ($user && password_verify($password, $user['password'])) {
+        // password is correct, start a session
+        $_SESSION['user_id'] = $user['researcher_id'];
+        $_SESSION['email'] = $user['email'];
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        // Invalid credentials
+        header("Location: login.html?error=Invalid email or password");
+        exit;
+    }
+} else {
+    // Invalid request method
+    header("Location: http://localhost/SWAP_Assignment/AMC_Site/public/login.php");
+    exit;
 }
 ?>
